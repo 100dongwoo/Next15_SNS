99a257430caa98d6d052d5a30f8859203898f99e
100dongwoo <qorehddn123@naver.com>
2025-01-18 19:52:18 +0900

    feat : tabProvider 셋팅추가

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,3 +1,23 @@
+import style from "./home.module.css";
+import Tab from "@/app/(afterLogin)/home/_component/Tab";
+import TabProvider from "@/app/(afterLogin)/home/_component/TabProvider";
+import PostForm from "@/app/(afterLogin)/home/_component/PostForm";
+import Post from "@/app/(afterLogin)/_component/Post";
+
 export default function Home() {
-  return <div>홈페이지</div>;
+  return (
+    <main className={style.main}>
+      <TabProvider>
+        <>
+          <Tab />
+          <PostForm />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+        </>
+      </TabProvider>
+    </main>
+  );
 }
