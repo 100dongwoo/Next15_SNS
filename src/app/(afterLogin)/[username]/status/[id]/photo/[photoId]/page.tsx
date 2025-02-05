677e2ae59cedbe0c1bb843b4fd69e981de0287b4
100dongwoo <qorehddn123@naver.com>
2025-02-05 09:39:29 +0900

    feat : 페이지관련 레이아웃 추가

---
+++ src/app/(afterLogin)/[username]/status/[id]/photo/[photoId]/page.tsx
@@ -0,0 +1,41 @@
+import Post from "@/app/(afterLogin)/_component/Post";
+import CommentForm from "@/app/(afterLogin)/[username]/status/[id]/_component/CommentForm";
+import ActionButtons from "@/app/(afterLogin)/_component/ActionButtons";
+import styles from "./photoModal.module.css";
+import PhotoModalCloseButton from "./_component/PhotoModalCloseButton";
+import { faker } from "@faker-js/faker";
+
+export default function Default() {
+  const photo = {
+    imageId: 1,
+    link: faker.image.urlLoremFlickr(),
+    Post: {
+      content: faker.lorem.text(),
+    },
+  };
+
+  return (
+    <div className={styles.container}>
+      <PhotoModalCloseButton />
+      <div className={styles.imageZone}>
+        <img src={photo.link} alt={photo.Post?.content} />
+        <div
+          className={styles.image}
+          style={{ backgroundImage: `url(${photo.link})` }}
+        >
+          <div className={styles.buttonZone}>
+            <div className={styles.buttonInner}>
+              <ActionButtons />
+            </div>
+          </div>
+        </div>
+        <div className={styles.commentZone}>
+          <Post />
+          <CommentForm />
+          <Post />
+          <Post />
+        </div>
+      </div>
+    </div>
+  );
+}
