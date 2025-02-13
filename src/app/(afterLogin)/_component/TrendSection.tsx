00fa65cd0b58fb3babb4ae7bb7772a79252d8802
100dongwoo <qorehddn123@naver.com>
2025-02-13 15:35:35 +0900

    feat : 로그아웃 & 로그인 여부에 따라 화면 다르게 하기

--- src/app/(afterLogin)/_component/TrendSection.tsx
+++ src/app/(afterLogin)/_component/TrendSection.tsx
@@ -2,27 +2,35 @@
 
 import style from "./trendSection.module.css";
 import Trend from "@/app/(afterLogin)/_component/Trend";
+import { useSession } from "next-auth/react";
 import { usePathname } from "next/navigation";
 
 export default function TrendSection() {
+  const { data } = useSession();
   const pathname = usePathname();
   if (pathname === "/explore") return null;
-
+  if (data?.user) {
+    return (
+      <div className={style.trendBg}>
+        <div className={style.trend}>
+          <h3>나를 위한 트렌드</h3>
+          <Trend />
+          <Trend />
+          <Trend />
+          <Trend />
+          <Trend />
+          <Trend />
+          <Trend />
+          <Trend />
+          <Trend />
+          <Trend />
+        </div>
+      </div>
+    );
+  }
   return (
     <div className={style.trendBg}>
-      <div className={style.trend}>
-        <h3>나를 위한 트렌드</h3>
-        <Trend />
-        <Trend />
-        <Trend />
-        <Trend />
-        <Trend />
-        <Trend />
-        <Trend />
-        <Trend />
-        <Trend />
-        <Trend />
-      </div>
+      <div className={style.noTrend}>트렌드를 가져올 수 없습니다.</div>
     </div>
   );
 }
