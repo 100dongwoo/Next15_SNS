2a0bea81ba7af7612f74d07eef5b18ae9532c175
100dongwoo <qorehddn123@naver.com>
2025-02-13 16:45:43 +0900

    feat : react-query 셋팅 추가

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -2,22 +2,34 @@ import style from "./home.module.css";
 import Tab from "@/app/(afterLogin)/home/_component/Tab";
 import TabProvider from "@/app/(afterLogin)/home/_component/TabProvider";
 import PostForm from "@/app/(afterLogin)/home/_component/PostForm";
-import Post from "@/app/(afterLogin)/_component/Post";
 
-export default function Home() {
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+import PostRecommends from "./_component/PostRecommends";
+import { getPostRecommends } from "./_lib/getPostRecommends";
+
+export default async function Home() {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ["posts", "recommends"],
+    queryFn: getPostRecommends,
+  });
+  const dehydratedState = dehydrate(queryClient);
+
   return (
     <main className={style.main}>
-      <TabProvider>
-        <>
-          <Tab />
-          <PostForm />
-          <Post />
-          <Post />
-          <Post />
-          <Post />
-          <Post />
-        </>
-      </TabProvider>
+      <HydrationBoundary state={dehydratedState}>
+        <TabProvider>
+          <>
+            <Tab />
+            <PostForm />
+            <PostRecommends />
+          </>
+        </TabProvider>
+      </HydrationBoundary>
     </main>
   );
 }
